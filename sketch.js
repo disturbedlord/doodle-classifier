abd608575bf1d944e9d00be0f7eca6f61e9bd195
avi <avi@kali.avi.com>
2020-06-07 23:48:16 +0530

    Training the model

--- sketch.js
+++ sketch.js
@@ -37,6 +37,49 @@ function prepareData(category, data, label) {
   }
 }
 
+function trainEpoch(training) {
+  shuffle(training, true);
+
+  // train for one EPOCH
+  for (let i = 0; i < training.length; i++) {
+    let data = training[i];
+    let inputs = data.map(x => x / 255.0);
+
+    let label = training[i].label;
+    let targets = [0, 0, 0];
+    targets[label] = 1;
+
+    nn.train(inputs, targets);
+  }
+
+  log("Completed one EPOCH");
+}
+
+function testAll(testing) {
+  let correct = 0;
+  for (let i = 0; i < testing.length; i++) {
+    let data = testing[i];
+    let inputs = data.map(x => x / 255.0);
+
+    let label = testing[i].label;
+    let guess = nn.predict(inputs);
+
+    // console.log(guess);
+    let m = max(guess);
+    let classification = guess.indexOf(m);
+    // console.log(classification);
+    //
+    // console.log(label);
+
+    if (classification == label)
+      correct++;
+
+  }
+
+  return correct / testing.length;
+
+}
+
 function setup() {
   createCanvas(280, 280);
   background(0);
@@ -52,23 +95,17 @@ function setup() {
   training = training.concat(rainbows.training);
   training = training.concat(trains.training);
 
-  shuffle(training, true);
+  let testing = [];
+  testing = testing.concat(cats.training);
+  testing = testing.concat(rainbows.training);
+  testing = testing.concat(trains.training);
 
-  // train for one EPOCH
-  for (let i = 0; i < training.length; i++) {
-    let data = training[i];
-    let inputs = [];
-    for (let j = 0; j < data.length; j++) {
-      inputs[j] = data[j] / 255.0;
-    }
-    let label = training[i].label;
-    let targets = [0, 0, 0];
-    targets[label] = 1;
 
-    nn.train(inputs, targets);
-  }
+  trainEpoch(training);
 
-  log("Completed one EPOCH");
+  let percent = testAll(testing);
+
+  print("% Correct: " + percent);
 
   // display doodle's
   // let total = 100;
